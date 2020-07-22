172a799b28028c9a5edfb6765f24a4b24ca4c77b
Youngjun Yoo <yoo156@purdue.edu>
2020-07-23 05:02:12 +0900

    Create Sum_and_Difference_of_Two_Numbers.c

---
+++ Sum_and_Difference_of_Two_Numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	  int a, b;
+    float c, d;
+
+    scanf("%d %d\n", &a, &b);
+    scanf("%f %f\n", &c, &d);
+    
+    printf("%d %d\n", a + b, a - b);
+    printf("%.1f %.1f\n", c + d, c - d);
+
+    return 0;
+}
+
+/*Sample Input
+
+10 4
+4.0 2.0
+Sample Output
+
+14 6
+6.0 2.0*?
+
